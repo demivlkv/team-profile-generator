eb08090f11aa4612a34bda6922aafb13751c6308
Demi Hayashi <demi.h@me.com>
2022-08-09 20:13:13 -0700

    edit prompts flow according to user story

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// team profiles
-const Employee = require('./lib/Employee.js');
+// get team profiles
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
@@ -11,15 +10,20 @@ const Intern = require('./lib/Intern.js');
 // push employee info into array
 const teamArray = [];
 
-// prompts user to add name, id, & email of employee
-const addEmployee = () => {
+// prompts user to add manager info
+const addManager = () => {
+    console.log(`
+==================
+Add a Team Manager
+==================
+	`);
 	return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of the employee?',
-            validate: nameInput => {
-                if (nameInput) {
+            message: 'What is the name of the team manager?',
+            validate: managerName => {
+                if (managerName) {
                     return true;
                 } else {
                     console.log('Please enter a name!');
@@ -29,21 +33,21 @@ const addEmployee = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'Please enter an ID number of the employee.',
-            validate: idInput => {
-                if (idInput) {
+            message: "Please enter the team manager's ID number.",
+            validate: managerId => {
+                if (managerId) {
                     return true;
                 } else {
-                    console.log('Please enter a valid number!');
+                    console.log('Please enter a valid ID number!');
                 }
             }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter the e-mail address of the employee.',
-            validate: emailInput => {
-                if (emailInput) {
+            message: 'Please enter the e-mail address of the team manager.',
+            validate: managerEmail => {
+                if (managerEmail) {
                     return true;
                 } else {
                     console.log('Please enter an e-mail address!');
@@ -51,19 +55,82 @@ const addEmployee = () => {
             }
         },
         {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the team manager's office number.",
+            validate: managerOfficeNumber => {
+                if (managerOfficeNumber) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid number!');
+                }
+            }
+        },
+        {
             type: 'list',
             name: 'role',
-            message: 'What is the role of the employee?',
-            choices: ['Manager', 'Engineer', 'Intern']
+            message: 'Would you like to add a team member?',
+            choices: ['Engineer', 'Intern', 'Finish Building Team']
+        }
+    ])
+    .then(employeeData => {
+        console.log(employeeData);
+
+        if (employeeData.role === 'Engineer') {
+            addEngineer();
+        } else if (employeeData.role === 'Intern') {
+            addIntern();
+        } else {
+            return '';
+        }
+    })
+};
+
+// display questions for engineer info
+const addEngineer = () => {
+    console.log(`
+===============
+Add an Engineer
+===============
+	`);
+	return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?',
+            validate: engineerName => {
+                if (engineerName) {
+                    return true;
+                } else {
+                    console.log('Please enter a name!');
+                }
+            }
         }
     ])
 };
 
-// if employee role is manager, then ask these questions
-
-// if employee role is engineer, then ask these questions
-
-// if employee role is intern, then ask these questions
+// display questions for intern info
+const addIntern = () => {
+    console.log(`
+=============
+Add an Intern
+=============
+	`);
+	return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the intern?',
+            validate: internName => {
+                if (internName) {
+                    return true;
+                } else {
+                    console.log('Please enter a name!');
+                }
+            }
+        }
+    ])
+};
 
 // initialize app
-addEmployee();
+addManager();
